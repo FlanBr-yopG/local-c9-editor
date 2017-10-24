dd186914acf2305fe6db69f18b6511ce6b392bc9
Brian Flannery <brian.flannery@pearson.com>
2017-10-23 21:15:05 -0600

    Better way to log stderr.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 container=c9
 image=yopgflanbr-node
-version=0.0.24
+version=0.0.25
 latest_c9_core_commit=$(git ls-remote git://github.com/c9/core HEAD | awk '{print $1}')
 
 main() {
@@ -10,7 +10,7 @@ main() {
 FROM node
 ENV NODE_ENV=production
 # Avoid "Error: Cannot find module 'amd-loader'":
-RUN npm i -g npm@4.6.1 > /tmp/npm_i_g_npm_4_6_1.log
+RUN npm i -g npm@4.6.1 > /tmp/npm_i_g_npm_4_6_1.log 2>&1
 RUN yum -y install git || apt-get -y install git
 RUN mkdir /home/node/app; chown node /home/node/app
 RUN mkdir /w; chown node /w
@@ -19,10 +19,10 @@ WORKDIR /home/node/app
 ARG latest_c9_core_commit
 RUN git init; git remote add origin git://github.com/c9/core.git; git fetch origin; \
     git checkout $latest_c9_core_commit; \
-    scripts/install-sdk.sh > /tmp/scripts_install_sdk.log ; \
+    scripts/install-sdk.sh > /tmp/scripts_install_sdk.log 2>&1 ; \
     git reset HEAD --hard;
 USER root
-RUN npm i -g npm > /home/node/app/npm_i_g_npm.log
+RUN npm i -g npm > /home/node/app/npm_i_g_npm.log 2>&1
 USER node
 # Stop c9 from forcing authentication:
 RUN sed -i -e 's_127.0.0.1_0.0.0.0_g' configs/standalone.js
@@ -30,7 +30,7 @@ RUN sed -i -e 's_127.0.0.1_0.0.0.0_g' configs/standalone.js
 EXPOSE 8181
 ENTRYPOINT ["node", "server.js", "-w", "/w", "--listen", "0.0.0.0"]
 # CMD ["-a", "user1:pass1", "--debug"]
-CMD ["--debug"
+CMD ["--debug"]
 EOFdf
   set -exu
   local already_image=$(docker images -f=reference="$image:$version" --format '{{.ID}}')
